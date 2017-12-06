e20fae2fd6484d736a38d516c5d3e8c2dc6b5dd8
deek25 <deekshaagrawal.da25@gmail.com>
2017-12-06 17:46:07 +0530

    modal implementation

--- js/main.js
+++ js/main.js
@@ -27,7 +27,18 @@ function logout() {
 	window.location.href = "login.html";
 	localStorage.removeItem("pratChatToken");
 }
+function random(){
+	$('#addreminder').modal();
+}
 
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
 function showContactProfile() {
 	$('#cprof').css('z-index', '300');
 	$('#chat').css('position', 'absolute');
